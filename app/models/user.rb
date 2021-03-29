7da10b901673f650e6c8417c2eae398ad47e6509
mboya <mboyaberry@gmail.com>
2021-03-29 09:29:11 +0300

    paper trail for context versioning and whodunnit  and scope

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,11 @@
 class User < ApplicationRecord
+  has_paper_trail
+
   has_secure_password
 
   validates :email,
     presence: true,
     uniqueness: true
+
+  scope :active, -> { where(archive: false) }
 end
