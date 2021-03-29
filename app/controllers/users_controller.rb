efe91e8be25ebc176d02eae23cb3b3e41b75c936
mboya <mboyaberry@gmail.com>
2021-03-29 09:27:41 +0300

    Controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,30 @@
 class UsersController < ApplicationController
   def index
-    render jsonapi: User.all
+    render jsonapi: User.active
+  end
+
+  def archive
+    user = find_user
+    if user
+      user&.update(archive: !user.archive)
+
+      NotifieerJob.set(wait: 20.second).perform_later(user)
+
+      render jsonapi: user
+    else
+      render json: {
+        error: 'Record not Found'
+      }, status: :not_found
+    end
+  end
+
+  private
+
+  def id
+    params.dig(:user_id)
+  end
+
+  def find_user
+    User.find_by(id: id)
   end
 end
